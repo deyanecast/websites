4191db648eb74845dbbf312b65e229dc773507e6
AlejandraArroyo <arroyoalejandra97@gmail.com>
2022-03-30 20:50:46 -0600

    Update prueba.php
    .

--- prueba.php
+++ prueba.php
@@ -9,6 +9,6 @@ $header = "Enviado desde la pagina ";
 $mensajecompleto = $mensaje. "Atentamente".$nombre;
 
 mail($destino,$asunto,$mensajecompleto,$header);
-echo "<script>alert('correo enviado')</script>";
-echo "<script>setTimeout(\"location.href='index.html'\",1000)</script>";
+echo "hola";
+
 ?>
