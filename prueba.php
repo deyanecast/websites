dcbfc1a4c0c3cffe2cf1e1fc89ea6e8d1f46af95
AlejandraArroyo <arroyoalejandra97@gmail.com>
2022-04-10 20:23:26 -0600

    Update prueba.php

--- prueba.php
+++ prueba.php
@@ -1,4 +1,32 @@
 <?php
+ob_start();
+header("Cache-control: private, no-cache");
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header("Pragma: no-cache");
+header("Cache: no-cahce");
+ini_set('max_execution_time', 90000);
+ini_set("memory_limit", -1);
+
+$request = $_REQUEST["request"]; 
+switch($request){
+	case "grabar":
+		$codigo = $_REQUEST["nombre"];
+		enviar();
+		break;
+        default:
+		$arr_respuesta = array(
+			"status" => false,
+			"data" => [],
+			"message" => "Seleccione un metodo..."
+		);
+		echo json_encode($arr_respuesta);
+
+    }      
+			
+
+
+function enviar()
+{
 $destino = 'arroyoalejandra97@gmail.com';
 $nombre = 'alejandra';
 $asunto = 'pruebas';
@@ -9,6 +37,5 @@ $header = "Enviado desde la pagina ";
 $mensajecompleto = $mensaje. "Atentamente".$nombre;
 
 mail($destino,$asunto,$mensajecompleto,$header);
-echo "hola";
-
+}
 ?>
