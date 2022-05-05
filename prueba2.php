e6c89585e8cc9018135aadc11c05b8ce7404ff0d
AlejandraArroyo <arroyoalejandra97@gmail.com>
2022-05-05 16:16:53 -0600

    eliminaPruebas

--- prueba2.php
+++
@@ -1,9 +0,0 @@
-<?php
-$hola = <<<'EOT'
-Mi nombre es "$nombre". Estoy escribiendo un poco de $foo->foo.
-Ahora, estoy escribiendo un poco de {$foo->bar[1]}.
-Esto debería mostrar una 'A' mayúscula: \x41
-EOT;
-echo $hola;
-?>
-?>
